eec16d0fa0134a0afb7e6cb64b9daafb201501c2
Steve Klabnik <steve@steveklabnik.com>
2015-07-24 12:43:33 -0400

    use the gcc crate
    much, much nicer. and more featureful\!

--- build.rs
+++ build.rs
@@ -1,30 +1,5 @@
-use std::process::Command;
-use std::env;
-use std::path::Path;
-use std::io::ErrorKind;
-
-fn run(cmd: &mut Command) {
-    println!("running: {:?}", cmd);
-    let status = match cmd.status() {
-        Ok(status) => status,
-        Err(ref e) if e.kind() == ErrorKind::NotFound => {
-            panic!(format!("failed to execute command: {}", e));
-        }
-        Err(e) => panic!(format!("failed to execute command: {}", e)),
-    };
-    if !status.success() {
-        panic!(format!("command did not execute successfully, got: {}", status));
-    }
-}
+extern crate gcc;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
-    run(Command::new("g++").args(&["src/v8_glue.c", "-c", "-fPIC", "-o"])
-                       .arg(&format!("{}/v8_glue.o", out_dir)));
-    run(Command::new("ar").args(&["crus", "libv8_glue.a", "v8_glue.o"])
-                      .current_dir(&Path::new(&out_dir)));
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=v8_glue");
+    gcc::Config::new().cpp(true).file("src/v8_glue.c").compile("libv8_glue.a");
 }
